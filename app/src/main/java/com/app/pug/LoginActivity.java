a06dc59ddce7099527c453bfd450469b62e6cf85
mativodominic <dmjandom6@gmail.com>
2015-02-20 11:48:51 +0300

    A few changes in home screen, though not complete.

--- app/src/main/java/com/app/pug/LoginActivity.java
+++ app/src/main/java/com/app/pug/LoginActivity.java
@@ -20,6 +20,7 @@ public class LoginActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_login);
         setContentView(R.layout.drawer_layout);
 
         cropProfileImage();
